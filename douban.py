338c37fdcdd737d21d8e98296d06784e84b2297f
skyline75489 <skyline75489@outlook.com>
2014-07-21 14:52:31 +0800

    添加若干API函数，并重构代码

--- douban.py
+++ douban.py
@@ -1,17 +1,34 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import requests
 
 class Douban:
     """ Douban class, provides some APIs.
     """
-    base_url = 'http://www.douban.com'
-    app_name = 'radio_desktop_win'
-    version = '100'
-
     def __init__(self):
+        self.login_url = 'http://www.douban.com/j/app/login'
+        self.channel_url = 'http://www.douban.com/j/app/radio/channels'
+        self.api_url = 'http://www.douban.com/j/app/radio/people'
+        self.app_name = 'radio_desktop_win'
+        self.version = '100'
+        self.typeMap = {
+            'new': 'n',
+            'playing': 'p',
+            'rate': 'r',
+            'unrate': 'u'
+            'end': 'e',
+            'bye': 'b',
+            'skip': 's',
+        }
+        
+        self.user_id = None
+        self.expire = None
+        self.token = None
+        self.user_name = None
         self.channels = None
+        self.email = None
+        self.password = None
+        
         try:
             import config
         except ImportError:
@@ -26,44 +43,83 @@ class Douban:
         self.email = input("Please input your email: ")
         self.password = getpass.getpass('password: ')
     
-    def test_login(self):
+    def _do_api_request(self, sid=None, channel=None, kbps=64, _type):
+        payload = {'app_name': self.app_name, 'version': self.version, 'user_id': self.user_id, 
+                'expire': self.expire, 'token': self.token, 'sid': sid, 'h': '','channel': channel, 'type': _type}
+        
+        r = requests.get(self.api_url, params=payload)
+        return r
+        
+    def user_login(self):
         if not (hasattr(self, 'email') and hasattr(self, 'password')):
             self.login()
         payload = {'email': self.email, 'password': self.password, 'app_name': self.app_name, 'version': self.version}
-        r = requests.post(self.base_url + '/j/app/login', params=payload, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+        r = requests.post(self.login_url, params=payload, headers={'Content-Type': 'application/x-www-form-urlencoded'})
         if r.json()['r'] == 0:
             self.user_id = r.json()['user_id']
             self.expire = r.json()['expire']
-            self.user_token = r.json()['token']
+            self.token = r.json()['token']
             self.user_name = r.json()['user_name']
             return True
         else:
             return False
-
+            
+    def _get_type(self, option):
+        return self.typeMap[option]
+        
     def get_channels(self):
         """ Return a list of channels
         """
         if self.channels is None:
-            channels_url = self.base_url + '/j/app/radio/channels'
-            r = requests.get(channels_url)
+            r = requests.get(self.channel_url)
             # Cache channels
             self.channels = r.json()['channels']
             return self.channels
         return self.channels
 
-    def get_type(self):
-        return 'n'
-
-    def get_song_list(self, channel_id):
-        song_list_url = self.base_url + '/j/app/radio/people'
-        if self.test_login():
-            payload = {'app_name': self.app_name, 'version': self.version, 'user_id': self.user_id, 
-                'expire': self.expire, 'token': self.user_token, 'channel': channel_id, 'type': self.get_type()}
-        else:
-            payload = {'app_name': self.app_name, 'version': self.version, 'channel': channel_id, 'type': self.get_type()}
-        r = requests.get(song_list_url, params=payload)
-
-        # 0: success, 1: error
+    def get_new_play_list(self, channel, kbps=64):
+        _type = self._get_type('new')
+        payload = {'app_name': self.app_name, 'version': self.version, 'user_id': self.user_id, 
+                'expire': self.expire, 'token': self.token, 'sid': '', 'h': '','channel': channel, 'kbps': kbps, 'type': _type}
+        
+        r = requests.get(self.api_url, params=payload)
         if r.json()['r'] == 0:
             songs = r.json()['song']
             return songs
+            
+    def get_playing_list(self, channel, kbps=64):
+        _type = self._get_type('playing')
+        payload = {'app_name': self.app_name, 'version': self.version, 'user_id': self.user_id, 
+                'expire': self.expire, 'token': self.token, 'sid': '', 'h': '','channel': channel, 'kbps': kbps, 'type': _type}
+        
+        r = requests.get(self.api_url, params=payload)
+        if r.json()['r'] == 0:
+            songs = r.json()['song']
+            return songs
+
+    def rate_song(self, sid, channel):
+        _type = self._get_type('rate')
+        r = self._do_api_request(sid=sid, channel=channel, _type=_type)
+        return r
+    
+    def _unrate_song(self, sid, channel):
+        _type = self._get_type('unrate')
+        r = self._do_api_request(sid=sid, channel=channel, _type=_type)
+        return r
+        
+    def _skip_song(self, sid, channel):
+        _type = self._get_type('skip')
+        r = self._do_api_request(sid=sid, channel=channel, _type=_type)
+        return r
+        
+    def _end_song(self, sid, channel):
+        _type = self._get_type('end')
+        r = self._do_api_request(sid=sid, channel=channel, _type=_type)
+        return r
+        
+    def _bye_song(self, sid, channel):
+        _type = self._get_type('bye')
+        r = self._do_api_request(sid=sid, channel=channel, _type=_type)
+        return r
+        
+        
