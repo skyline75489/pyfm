e17746324bbdd2cf0cf9aaa2fe7404efa1b7e1a1
skyline75489 <skyline75489@outlook.com>
2014-07-21 15:35:32 +0800

    Extend playlist when current list ended

--- pyfm.py
+++ pyfm.py
@@ -1,7 +1,8 @@
 # /usr/bin/env python3
 import os
-import subprocess
 import sys
+import subprocess
+import time
 from collections import deque
 
 from douban import Douban
@@ -14,7 +15,7 @@ class Pyfm:
         self.player = Player()
         self.current_channel = 0
         self.current_song = None
-        self.current_song_list = None
+        self.current_play_list = None
         
     def _list_channels(self):
         channels = self.douban.get_channels()
@@ -23,20 +24,27 @@ class Pyfm:
     
     def _choose_channel(self, channel):
         self.current_channel = channel
-        self.current_song_list = deque(self.douban.get_song_list(self.current_channel))
+        self.current_play_list = deque(self.douban.get_new_play_list(self.current_channel))
         
     def _play(self):
         while True:
-            song = self.current_song_list.popleft()
-            self.current_song = Song(song)
+            _song = self.current_song_list.popleft()                
+            self.current_song = Song(_song)
+            
+            # Currently playing the last song in queue
+            if len(self.current_play_list) == 0:
+                # Extend playlist
+                playing_list = self.douban.get_playing_list(self.current_song.sid, self.current_channel)
+                self.current_song_list.extend(deque(playing_list))
+                
             self.player.play(self.current_song)
             # player process exit unsucessful
             if self.player.return_code != 0:
                 print("Something wrong happens while playing...")
                 return 
-            self.current_song_list.append(self.douban.get_song_list(self.current_channel)[0])
+                
+            self.player.bye_song(self.current_song.sid, self.current_channel)
             
-    
     def start(self):
         # self._list_channels()
         self._choose_channel(2)
