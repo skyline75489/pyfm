02ef5789e567913b6fb0d973d9c19f003960e392
skyline75489 <skyline75489@outlook.com>
2014-07-21 14:53:34 +0800

    subprocess.call -> subprocess.Popen

--- player.py
+++ player.py
@@ -5,13 +5,17 @@ class Player:
     def __init__(self):
         self.is_playing = False
         self.current_song = None
-        self.return_code = None
+        self.player_process = None
+        self.return_code = 0
         
     def play(self, song):
         self.current_song = song
-        # using mpg123 to play music, -q(quiet) to remove the output in stdout
+        # using mpg123 to play music
+        # -q(quiet) remove the output in stdout
+        # -C(control) enables the keyboard control
         print("Now playing: " + str(self.current_song.artist) + " - " + str(self.current_song.title))
-        self.return_code = subprocess.call(["mpg123", "-q", "-C", self.current_song.url])
+        self.player_process = subprocess.Popen(["mpg123", "-q", "-C", self.current_song.url])
+        self.return_code = self.player_process.returncode
         self.is_playing = True
         
     def stop(self):
