433c9790f9d3319f5f8771373b53fe887fa39362
skyline75489 <skyline75489@outlook.com>
2014-07-27 22:24:52 +0800

    make all-uppercase title normal

--- song.py
+++ song.py
@@ -2,8 +2,13 @@ class Song:
     def __init__(self, song_json):
         self.artist = song_json['artist']
         self.title = song_json['title']
+        # Uppercase title, make it normal
+        if self.title.isupper():
+            self.title = self.title.title
+        
         self.album_title = song_json['albumtitle']
         self.length_in_sec = song_json['length']
+        
         # Process the length of the song
         self.length_minute = divmod(self.length_in_sec, 60)[0]
         self.length_sec = divmod(self.length_in_sec, 60)[1]
