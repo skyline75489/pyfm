bb785cbba257aa5e50f5033ad3243525078aa4ea
skyline75489 <skyline75489@outlook.com>
2014-08-06 09:23:20 +0800

    fix crash when song info is incomplete

--- song.py
+++ song.py
@@ -1,23 +1,26 @@
 class Song:
     def __init__(self, song_json):
-        self.artist = song_json['artist']
-        self.title = song_json['title']
-        # Uppercase title, make it normal
-        if self.title.isupper():
-            self.title = self.title.title
+        try:
+            self.artist = song_json['artist']
+            self.title = song_json['title']
+            # Uppercase title, make it normal
+            if self.title.isupper():
+                self.title = self.title.title
         
-        self.album_title = song_json['albumtitle']
-        self.length_in_sec = song_json['length']
+            self.album_title = song_json['albumtitle']
+            self.length_in_sec = song_json['length']
         
-        # Process the length of the song
-        self.length_minute = divmod(self.length_in_sec, 60)[0]
-        self.length_sec = divmod(self.length_in_sec, 60)[1]
-        self.length_in_str = str(self.length_minute) + ":" + str(self.length_sec)
+            # Process the length of the song
+            self.length_minute = divmod(self.length_in_sec, 60)[0]
+            self.length_sec = divmod(self.length_in_sec, 60)[1]
+            self.length_in_str = str(self.length_minute) + ":" + str(self.length_sec)
         
-        self.like = True and song_json['like'] == 1 or False
-        self.url = song_json['url']
-        self.ssid = song_json['ssid']
-        self.sid = song_json['sid']
-        self.aid = song_json['aid']        
-    
+            self.like = True and song_json['like'] == 1 or False
+            self.url = song_json['url']
+            self.sid = song_json['sid']
+            self.aid = song_json['aid']
+            self.ssid = song_json['ssid']
+        except KeyError:
+            pass
+            
         
